17bf67a05e518ee2b7abf444f8ce0de2d170fc8d
kimkundad <kim.kundad@gmail.com>
2018-09-02 10:58:29 +0700

    golf

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -16,7 +16,7 @@
             <div class="container">
                 <nav aria-label="breadcrumb">
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Payment</li>
                     </ul>
                 </nav>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -51,7 +51,7 @@
             <div class="container">
                 <nav aria-label="breadcrumb">
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">{{$objs->pro_name}}</li>
                     </ul>
                 </nav>
