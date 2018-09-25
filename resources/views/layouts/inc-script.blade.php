2fa28381ee661fc62e19381734ff08b5bfe57eb8
kimkundad <kim.kundad@gmail.com>
2018-09-25 08:56:36 +0700

    golf

--- resources/views/layouts/inc-script.blade.php
+++ resources/views/layouts/inc-script.blade.php
@@ -38,3 +38,12 @@
                     });
                });
             </script>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126404865-1"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-126404865-1');
+            </script>
