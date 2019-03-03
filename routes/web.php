83e6d97385d908fd87f00a02780216a0966a589d
kimkundad <kim.kundad@gmail.com>
2019-03-03 12:47:19 +0700

    golf

--- routes/web.php
+++ routes/web.php
@@ -98,5 +98,6 @@ Route::group(['middleware' => 'admin'], function() {
   Route::resource('admin/blog', 'BlogController');
   Route::post('api/api_blog_status', 'BlogController@api_blog_status');
   Route::post('admin/file/posts', 'BlogController@imagess');
+  Route::resource('admin/contact_admin', 'EnvelopeController');
 
   });
